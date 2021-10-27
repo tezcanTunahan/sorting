94dd3c742f5565659d696ce452cc1018dc96f559
Tunahan Tezcan <ttezcan.1999@gmail.com>
2021-10-27 20:40:15 +0300

    try

--- src/components/SortingVisualizer/SortingVisualizer.jsx
+++ src/components/SortingVisualizer/SortingVisualizer.jsx
@@ -30,15 +30,6 @@ function SortingVisualizer(){
     }
 
 
-
-
-
-
-
-
-
-
-
     //from https://stackoverflow.com/questions/1527803/generating-random-whole-numbers-in-javascript-in-a-specific-range
     function getRandomArbitrary(min, max) {
         return Math.floor(Math.random() * (max - min + 1) + min)
--- src/sortingAlgorithms/sortingAlgorithms.js
+++ src/sortingAlgorithms/sortingAlgorithms.js
@@ -1,21 +1,14 @@
 
-function bubbleSort(arr,animatedArr){
+function  bubbleSort  (arr,animatedArr){
     
     const arrayBars = document.getElementsByClassName("array-bar")
     for(let i = 0; i < arr.length; i++){
-        for(let j = 0; j < arr.length; j++){
+        for(let j = 0; j < arr.length -i; j++){
             if(arr[j] > arr[j +1 ]){
-                let temp = arr[j]   
-                arr[j] = arr[j+1]
-                arr[j+1] = temp
-                
                 setTimeout(()=>{
-                    arrayBars[i].style.backgroundColor = "red" 
-                    arrayBars[j].style.backgroundColor = "red" 
-                    // let tempHeight = arr[j].style.height  
-                    // arrayBars[j].style.height = arrayBars[i].style.height
-                    // arrayBars[j+1].style.height = tempHeight
-
+                    swap(arr,j)
+                    arrayBars[i].style.backgroundColor = "black" 
+                    arrayBars[j].style.backgroundColor = "black" 
                     setTimeout(()=>{
                         arrayBars[i].style.backgroundColor = "blueviolet" 
                         arrayBars[j].style.backgroundColor = "blueviolet"  
@@ -27,6 +20,11 @@ function bubbleSort(arr,animatedArr){
     return arr
 }
 
+function swap(arr,j){
+    let temp = arr[j]   
+    arr[j] = arr[j+1]
+    arr[j+1] = temp
+}
 
 
 
